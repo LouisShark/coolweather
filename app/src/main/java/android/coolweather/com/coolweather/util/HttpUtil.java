8f84f88112317dedc59498239c552d048bd28131
LouisShark <13185137187@163.com>
2017-03-09 00:11:00 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/android/coolweather/com/coolweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package android.coolweather.com.coolweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by LouisShark on 2017/3/8.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
