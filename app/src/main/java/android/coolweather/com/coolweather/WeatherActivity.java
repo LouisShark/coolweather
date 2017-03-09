5f34f04b742cccc9433959cd3d505d072320d2fa
LouisShark <13185137187@163.com>
2017-03-10 00:24:47 +0800

    完成天气预报功能

---
+++ app/src/main/java/android/coolweather/com/coolweather/WeatherActivity.java
@@ -0,0 +1,126 @@
+package android.coolweather.com.coolweather;
+
+import android.content.SharedPreferences;
+import android.coolweather.com.coolweather.gson.Forecast;
+import android.coolweather.com.coolweather.gson.Weather;
+import android.coolweather.com.coolweather.util.HttpUtil;
+import android.coolweather.com.coolweather.util.Utility;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class WeatherActivity extends AppCompatActivity {
+    private ScrollView weatherLayout;
+    private LinearLayout forecastLayout;
+    private TextView titleCity, titleUpdateTime, degreeText, weatherInfoText, aqiText, pm25Text, comfortText, carWashText, sportText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
+        forecastLayout = (LinearLayout) findViewById(R.id.forecast_layout);
+        titleCity = (TextView) findViewById(R.id.title_city);
+        titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
+        degreeText = (TextView) findViewById(R.id.degree_text);
+        weatherInfoText = (TextView) findViewById(R.id.weather_info_text);
+        aqiText = (TextView) findViewById(R.id.api_text);
+        pm25Text = (TextView) findViewById(R.id.pm25_text);
+        comfortText = (TextView) findViewById(R.id.comfort_text);
+        carWashText = (TextView) findViewById(R.id.car_wash_text);
+        sportText = (TextView) findViewById(R.id.sport_text);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = prefs.getString("weather", null);
+        if (weatherString != null) {
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            showWeatherInfo(weather);
+        } else {
+            String weatherId = getIntent().getStringExtra("weather_id");
+            weatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(weatherId);
+        }
+
+    }
+
+    private void requestWeather(String weatherId) {
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=37cbdf6bc487414ea3cd2b3f403634d6";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Weather weather = Utility.handleWeatherResponse(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (weather != null && "ok".equals(weather.status)) {
+                            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                            editor.putString("weather", responseText);
+                            editor.apply();
+                            showWeatherInfo(weather);
+                        } else {
+                            Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void showWeatherInfo(Weather weather) {
+        String cityName = weather.basic.cityName;
+        String updateTime = weather.basic.update.updateTime.split(" ")[1];
+        String degree = weather.now.temperature + "℃";
+        String weatherInfo = weather.now.more.info;
+        titleCity.setText(cityName);
+        titleUpdateTime.setText(updateTime);
+        degreeText.setText(degree);
+        weatherInfoText.setText(weatherInfo);
+        forecastLayout.removeAllViews();
+        for (Forecast f : weather.forecastList) {
+            View v = LayoutInflater.from(this).inflate(R.layout.forecast_item, forecastLayout, false);
+            TextView dateText = (TextView) v.findViewById(R.id.date_text);
+            TextView infoText = (TextView) v.findViewById(R.id.info_text);
+            TextView maxText = (TextView) v.findViewById(R.id.max_text);
+            TextView minText = (TextView) v.findViewById(R.id.min_text);
+            dateText.setText(f.date);
+            infoText.setText(f.more.info);
+            maxText.setText(f.temperature.max);
+            minText.setText(f.temperature.min);
+            forecastLayout.addView(v);
+        }
+        if (weather.aqi != null) {
+            aqiText.setText(weather.aqi.city.aqi);
+            pm25Text.setText(weather.aqi.city.pm25);
+        }
+        String comfort = "舒适度：" + weather.suggestion.comfort.info;
+        String carWash = "洗车指数：" + weather.suggestion.carWash.info;
+        String sport = "运动建议：" + weather.suggestion.sport.info;
+        comfortText.setText(comfort);
+        sportText.setText(carWash);
+        sportText.setText(sport);
+        weatherLayout.setVisibility(View.VISIBLE);
+    }
+}
---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by LouisShark on 2017/3/9.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+
+}
